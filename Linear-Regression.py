38b217cb07833c3a9a529471317db60b46307fdf
Muhammad Mohsin <mohsinpu24@gmail.com>
2024-08-11 04:36:38 +0500

    Linear Regression

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -1,3 +1,32 @@
+
+# Importing Libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Loading the dataset
+data = pd.read_csv('Salary_Data.csv')
+X = data.iloc[: , :-1].values # X is predictor
+y = data.loc[: , -1].values # Y is response
+
+# Split dataset into test and traing set
+
+
+# Traing Linear Regression Model
+
+
+# Predicting test set results
+
+
+# Visualizing the Training set results
+
+# Visualizing the test set results
+
+
+
+
+
+
+                
